301bd62614bf94269ed74c432c66c1cff17636da
Learan-Canning <learancanning@mail.com>
2026-04-30 14:06:10 +0100

    Complete Book.java and User.java,(fields, constructor, getters/setters). Added books.csv and users.csv initial data.)

--- src/Book.java
+++ src/Book.java
@@ -7,6 +7,46 @@ public class Book {
     private boolean isBorrowed;
     private int loanCount;
 
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+
+    public int getLoanCount() {
+        return loanCount;
+    }
+
+    public void setLoanCount(int loanCount) {
+        this.loanCount = loanCount;
+    }
 
     // Constructor for above
     public Book(String id, String title, String author, boolean isBorrowed, int loanCount) {
@@ -16,4 +56,9 @@ public class Book {
         this.isBorrowed = isBorrowed;
         this.loanCount = loanCount;
         }
+
+        //Book Saving. - Converts the book into a CSV line so it can be saved to books.csv
+    public String toCSV() {
+        return id + "," + title + author + "," + isBorrowed + "," + loanCount;
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,65 @@
+import java.util.List;
+
+
 public class User {
+
+    //user details
+    private String username;
+    private String password;
+    private boolean isAdmin;
+    private List<String> loanedBooks;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public List<String> getLoanedBooks() {
+        return loanedBooks;
+    }
+
+    public void setLoanedBooks(List<String> loanedBooks) {
+        this.loanedBooks = loanedBooks;
+    }
+
+    public void addLoanedBook(String bookId) {
+        loanedBooks.add(bookId);
+    }
+
+    public void removeLoanedBook(String bookId) {
+        loanedBooks.remove(bookId);
+    }
+
+    // Convert user to CSV line
+    public String toCSV() {
+        String loans = String.join("|", loanedBooks);
+        return username + "," + password + "," + isAdmin + "," + loans;
+    }
+
+    //Constructor for above. - user data
+    public User(String username, String password, boolean isAdmin, List<String> loanedBooks) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.loanedBooks = loanedBooks;
+    }
 }
